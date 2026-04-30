ae2ee73206c279873479e67e0209e99a2896e0ac
Keziah Vickraman <keziahvickraman@gmail.com>
2026-05-01 01:26:43 +0800

    add scale_smu and theme_smu_dark

--- R/theme_smu.R
+++ R/theme_smu.R
@@ -26,3 +26,56 @@ theme_smu <- function() {
       legend.position  = 'bottom'
     )
 }
+
+
+# R/scale_smu.R
+
+#' SMU colour palette
+#' @export
+smu_colours <- c(
+  crimson  = "#C41230",
+  navy     = "#1a1a2e",
+  grey     = "#7a7a88",
+  white    = "#FFFFFF",
+  light    = "#f5f5f7"
+)
+
+#' SMU discrete colour scale
+#' @param ... Arguments passed to ggplot2::scale_colour_manual()
+#' @export
+scale_colour_smu <- function(...) {
+  ggplot2::scale_colour_manual(values = unname(smu_colours), ...)
+}
+
+#' SMU discrete fill scale
+#' @param ... Arguments passed to ggplot2::scale_fill_manual()
+#' @export
+scale_fill_smu <- function(...) {
+  ggplot2::scale_fill_manual(values = unname(smu_colours), ...)
+}
+
+
+
+# R/theme_smu_dark.R
+
+#' SMU dark ggplot2 theme
+#' @export
+theme_smu_dark <- function() {
+  ggplot2::theme_minimal(base_family = "Source Sans 3") +
+    ggplot2::theme(
+      plot.background  = ggplot2::element_rect(fill = "#1a1a2e", colour = NA),
+      panel.background = ggplot2::element_rect(fill = "#1a1a2e", colour = NA),
+      plot.title       = ggplot2::element_text(face   = "bold",
+                                               colour = "#C41230",
+                                               size   = 14),
+      plot.subtitle    = ggplot2::element_text(colour = "#7a7a88"),
+      axis.title       = ggplot2::element_text(face   = "bold",
+                                               colour = "#f5f5f7"),
+      axis.text        = ggplot2::element_text(colour = "#f5f5f7"),
+      panel.grid.major = ggplot2::element_line(colour = "#2e2e4e"),
+      panel.grid.minor = ggplot2::element_blank(),
+      legend.background = ggplot2::element_rect(fill = "#1a1a2e", colour = NA),
+      legend.text      = ggplot2::element_text(colour = "#f5f5f7"),
+      legend.position  = "bottom"
+    )
+}
